93a25cd251430ee6ec13557ba7a23977bd3bd820
light_dev <dgr8steve95@gmail.com>
2023-07-30 14:43:55 +0100

    css changes

--- src/Component/ProductsComponent/Products.jsx
+++ src/Component/ProductsComponent/Products.jsx
@@ -14,7 +14,7 @@ const Products = ({ products, onAddToCart }) => {
                 return (
                   <div
                     key={product.id}
-                    className="col-xs-12 col-sm-6 col-md-4 col-lg-3"
+                    className="col-xs-12 col-sm-6 col-md-4 col-lg-3 product-list"
                   >
                     <Product product={product} onAddToCart={onAddToCart} />
                     <hr />
